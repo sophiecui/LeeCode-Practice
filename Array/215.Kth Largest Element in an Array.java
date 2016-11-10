1464eda8c68858965271d22b0ab64b8cc0b16d5c
Sophie Cui <sweety0913@hotmail.com>
2016-11-09 19:48:26 -0800

    Create 215.Kth Largest Element in an Array.java
    Same with the #414 question, to find the 3rd max in a array

---
+++ Array/215.Kth Largest Element in an Array.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> q = new PriorityQueue<>(new Comparator<Integer>(){
+            @Override
+            public int compare(Integer a, Integer b){
+                if(a < b){
+                    return 1;
+                }
+                return -1;
+            }
+        });
+
+        for(int i = 0; i < nums.length; i++ ){
+            q.add(nums[i]);
+        }
+        
+        int i = 1;
+        while(!q.isEmpty() && i < k){
+            q.poll();
+            i++;
+        }
+        return q.peek();
+        
+    }
+}
